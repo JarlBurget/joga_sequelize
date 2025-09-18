52a720ac470b89aa3bccc0b494af997093945fb8
JarlBurget <jarl-ivor.burget@voco.ee>
2025-09-18 13:33:05 +0300

    Created connection to database

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 4001
 
-const PORT = process.env.PORT || 4000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
+
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('joga_sequelize', 'root', 'qwerty', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+// Test the database connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 // Example route that returns JSON
 app.get('/', (req, res) => {
